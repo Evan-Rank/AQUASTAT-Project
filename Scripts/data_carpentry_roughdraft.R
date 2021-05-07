441cd904d17f0e50be3d6941fe0b2f2496949bfb
Evan-Rank <ersmb3@gmail.com>
2021-05-07 16:26:21 -0700

    more data and a plot

--- Scripts/data_carpentry_roughdraft.R
+++ Scripts/data_carpentry_roughdraft.R
@@ -10,6 +10,10 @@ aquastat <- read_csv(here("Data", "aquastat_flat.csv"))
 view(aquastat)
 
 aquastat_wide <- aquastat %>% 
+  select(Area, Variable_Name, Year, Value) %>% 
   pivot_wider(names_from = Variable_Name, values_from = Value)
 
 view(aquastat_wide)
+
+ggplot(aquastat_wide, x="Area", y="SDG 6.4.2. Water Stress", color="Year")+
+  geom_bar()
